ea65fe25575c340339a1bccde270d806bd92be32
Uri Plutzer <uri.plutzer@gmail.com>
2025-12-29 00:46:11 +0200

    HW 7

--- Binomial.java
+++ Binomial.java
@@ -13,7 +13,17 @@ public class Binomial {
 	// Computes the Binomial function, basic version.
 	public static int binomial1(int n, int k) { 
 		//// Repplace the following comment with your code
-		return 0;
+		if (k > 0 ) 
+		{
+			return 0;	
+		}
+
+		if (k == 0 || n == 0) 
+		{
+			return 1;	
+		}
+		
+		return binomial(n - 1, k) + binomial(n -1 , k - 1);
 	 }
 	
 	// Computes the Binomial function, efficiently
@@ -22,7 +32,17 @@ public class Binomial {
 		//// and then initializes all its elements to -1.
 		//// It then calls binomial(n, k, memo), which does all the heavy lifiting.
 		//// Replace the following statement with your code.
-		return 0;
+		int[][] memo = new int[n + 1][k + 1];
+
+		for(int i = 0; i <= n; i++)
+		{
+			for(int j = 0; j <= k; j++)
+			{
+				memo[i][j] = -1;
+			}
+		}
+		
+		return binomial(n, k, memo);
 	}
 
 	private static int binomial(int n, int k, int[][] memo) {
--- Koch.java
+++ Koch.java
@@ -27,8 +27,30 @@ public class Koch {
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
-		StdDraw.line(x1, y1, x2, y2);
+		
 		//// Write the rest of your code below.
+		
+		if (n == 0) 
+		{
+			StdDraw.line(x1, y1, x2, y2);
+			return;
+		}
+
+		double xA = x1 + (x2 - x1) / 3;
+		double yA = y1 + (y2 - y1) / 3;
+
+		double xB = x1 + 2 * (x2 - x1) / 3;
+		double yB = y1 + 2 * (y2 - y1) / 3;
+
+		double xC = (Math.sqrt(3) * (y1 - y2) + x1 + x2) / 2;
+		double yC = (Math.sqrt(3) * (x2 - x1) + y1 + y2) / 2;
+
+		curve(n - 1, x1, y1, xA, yA);
+		curve(n - 1, xA, yA, xC, yC);
+		curve(n - 1, xC, yC, xB, yB);
+		curve(n - 1, xB, yB, x2, y2);
+
+
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
@@ -38,5 +60,13 @@ public class Koch {
 		StdDraw.setXscale(0, 1.1);
 		// Draws a Koch snowflake of depth n
 		//// Write the rest of your code below.
+		
+		double x1 = 0.1, y1 = 0.1;
+		double x2 = 1.0, y2 = 0.1;
+		double x3 = 0.55, y3 = 0.9;
+
+		curve(n, x1, y1, x2, y2);
+		curve(n, x2, y2, x3, y3);
+		curve(n, x3, y3, x1, y1);
 	}
 }
